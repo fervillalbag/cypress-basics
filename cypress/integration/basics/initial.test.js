0092a5ee242113b0ef1152b4fa8459c0f3418b1b
fervillalbag <21fervillalbag@gmail.com>
2022-01-20 08:41:13 -0300

    feat: cy.get

--- cypress/integration/basics/initial.test.js
+++ cypress/integration/basics/initial.test.js
@@ -3,6 +3,14 @@
 describe("Basic test", () => {
   it("We have correctly title", () => {
     cy.visit("https://nextjs.org/");
+
+    // cy.contains()
     cy.contains("Start Learning").should("exist");
+    cy.contains("Documentation").click();
+
+    // cy.get()
+    cy.get("div[id=__next]");
+    cy.get("div#__notnext").should("not.exist");
+    cy.get("[role=region]");
   });
 });
