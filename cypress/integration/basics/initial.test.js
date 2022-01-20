b5a11859c6e7e7a53a0e5b82f5d9158a66701cf8
fervillalbag <21fervillalbag@gmail.com>
2022-01-20 10:43:21 -0300

    feat: verify url page current

--- cypress/integration/basics/initial.test.js
+++ cypress/integration/basics/initial.test.js
@@ -14,10 +14,44 @@ describe("Basic test", () => {
     cy.get("[role=region]");
   });
 
-  it.only("We have correctly button text", () => {
+  it("We have correctly button text", () => {
     cy.visit("https://nextjs.org/");
     // cy.get("[title=Showcase]").should("have.text", "Showcase");
+    cy.viewport("iphone-6");
 
     cy.get("[title=Showcase]").should("have.text", "Showcase");
+  });
+
+  it("Looks like good", () => {
+    cy.visit("https://nextjs.org/");
+    cy.viewport("iphone-6");
+
+    cy.contains("Documentation").click();
+    // confirm current page correct
+    cy.contains("Getting Started").should("exist");
+    cy.get("input[type='search'][placeholder='Search...']")
+      .first()
+      .type("Hello world");
+
+    // cy.get("[id='email-input-field']").type("fervillalbag@gmail.com");
+  });
+
+  it("Home index page", () => {
+    cy.visit("https://nextjs.org/");
+
+    cy.contains("The React Framework for Production").should("exist");
+    cy.contains("Start Learning").should("exist");
+    cy.contains("License: MIT").should("exist");
+    cy.contains("The Web SDK").should("exist");
+  });
+
+  it.only("Verifying link url", () => {
+    cy.visit("https://nextjs.org/");
+
+    cy.contains("Documentation").click();
+    cy.url().should("include", "/docs");
+
+    cy.go("back");
+    cy.contains("Start Learning").should("exist");
   });
 });
