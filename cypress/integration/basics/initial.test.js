04f797a2de0134c9d74b0ed6274b1494f977e35d
fervillalbag <21fervillalbag@gmail.com>
2022-01-20 10:12:59 -0300

    feat: cy.should

--- cypress/integration/basics/initial.test.js
+++ cypress/integration/basics/initial.test.js
@@ -4,13 +4,20 @@ describe("Basic test", () => {
   it("We have correctly title", () => {
     cy.visit("https://nextjs.org/");
 
-    // cy.contains()
+    // ... cy.contains()
     cy.contains("Start Learning").should("exist");
-    cy.contains("Documentation").click();
+    // cy.contains("Documentation").click();
 
-    // cy.get()
+    // ... cy.get()
     cy.get("div[id=__next]");
     cy.get("div#__notnext").should("not.exist");
     cy.get("[role=region]");
+  });
+
+  it.only("We have correctly button text", () => {
+    cy.visit("https://nextjs.org/");
+    // cy.get("[title=Showcase]").should("have.text", "Showcase");
+
+    cy.get("[title=Showcase]").should("have.text", "Showcase");
   });
 });
